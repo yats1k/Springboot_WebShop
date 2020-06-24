2bffcc87ba8c45656656d9e73754deb6d92383e4
Dmytro Yatsenko1 <Dmytro_Yatsenko1@epam.com>
2020-06-24 19:17:40 +0300

    spring init project

---
+++ webshop/src/test/java/com/yatsenko/springboot/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatsenko.springboot.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
